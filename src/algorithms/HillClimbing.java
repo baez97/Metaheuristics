d5190c650adc3a9952ee4dad0992b5c5df441ab4
José Manuel Báez <baezsoriano97@gmail.com>
2018-12-26 11:42:19 +0100

    Implementing Hill Climbing algorithm

---
+++ src/algorithms/HillClimbing.java
@@ -0,0 +1,81 @@
+package algorithms;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import optimization.Configuration;
+import optimization.OptimizationAlgorithm;
+
+public class HillClimbing extends OptimizationAlgorithm {
+
+    @Override
+    public void search() {
+        boolean improves = true;
+        ArrayList<Configuration> neighbors;
+        double bestScore;
+        // Algorithms must call this function always!
+        initSearch();
+
+        // Generates all the configurations.
+        Configuration currentSolution;
+        
+        // Generates a configuration.
+        currentSolution = problem.genRandomConfiguration();
+        
+        bestScore = this.evaluate(currentSolution);
+        
+        while ( improves ) {
+            improves = false;
+            
+            neighbors = this.generateNeighbors(currentSolution);
+            
+            for ( Configuration neighbor : neighbors ) {
+                this.evaluate(neighbor);
+            }
+            
+            currentSolution = this.bestSolution;
+            
+            if ( bestScore < currentSolution.score() ) {
+                this.bestScore = currentSolution.score();
+                improves = true;
+            }
+        }
+        
+        stopSearch();
+    }
+
+    @Override
+    public void showAlgorithmStats() {
+        
+    }
+
+    @Override
+    public void setParams(String[] args) {
+    }
+    
+    public ArrayList<Configuration> generateNeighbors(Configuration configuration) {
+        HashSet<Configuration> result = new HashSet<>();
+
+        int length = configuration.getValues().length;
+        Configuration newConfig;
+        
+        for ( int i = 0; i < length; i++ ) {
+            for ( int j = 0; j < length && j!= i; j++ ) {
+                newConfig = new Configuration(this.swap(configuration.getValues(), i, j));
+                result.add(newConfig);
+            }
+        }
+
+        return new ArrayList<>(result);
+    }
+    
+
+    public int[] swap(int[] n, int i, int j) {
+        int[] m = n.clone();
+        int temp = m[i];
+        m[i] = m[j];
+        m[j] = temp;
+
+        return m;
+    }
+
+}
